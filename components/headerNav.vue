3b0b0d3d904501ca99f9f5e0b8b64820f1652ac8
nedumd <davidsonchinedum@gmail.com>
2021-09-28 10:39:25 +0100

    first phase

---
+++ components/headerNav.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="bg-blue-500">
+    <div
+      class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div class="flex justify-center">
+      <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
+        <a
+          href="/"
+          aria-label="Company"
+          title="Company"
+          class="inline-flex items-center lg:mx-auto"
+        >
+          <svg
+            class="w-8 text-teal-accent-400"
+            viewBox="0 0 24 24"
+            stroke-linejoin="round"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke="currentColor"
+            fill="none"
+          >
+            <rect x="3" y="1" width="7" height="12"></rect>
+            <rect x="3" y="17" width="7" height="6"></rect>
+            <rect x="14" y="1" width="7" height="6"></rect>
+            <rect x="14" y="11" width="7" height="12"></rect>
+          </svg>
+          <span
+            class="ml-3 text-xl font-bold text-gray-100"
+            >FourSquare  Weather</span
+          >
+        </a>
+      </div>
+      <div class="flex items-center md:space-x-4">
+				<div class="relative">
+					<span class="absolute inset-y-0 left-0 flex items-center pl-2">
+						<button type="submit" title="Search" class="p-1 focus:outline-none focus:ring">
+							<svg fill="currentColor" viewBox="0 0 512 512" class="w-4 h-4 text-coolGray-800">
+								<path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
+							</svg>
+						</button>
+					</span>
+					<input type="search" name="Search" placeholder="Search..." class="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none bg-coolGray-100 text-coolGray-800 focus:bg-coolGray-50">
+				</div>
+			</div>
+		</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <headerNav />
+  <br>
+  <weatherCard />
+  <br>
+  </div>
+</template>
+
+<script>
+export default {
+ mounted() {
+    document.body.style.backgroundColor = 'lightblue'
+  } 
+}
+</script>
+
+<style scoped>
+</style>
